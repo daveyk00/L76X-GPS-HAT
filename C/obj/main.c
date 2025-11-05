2a50d1c395312a779bdea257fa0e83ca4551663c
daveyk00 <114528910+daveyk00@users.noreply.github.com>
2025-11-05 21:26:15 +1000

    Refactor main.c by removing unused signal handler
    Removed the signal handler for SIGINT and related time calculations.

--- C/obj/main.c
+++ C/obj/main.c
@@ -2,37 +2,16 @@
 #include <stdlib.h>     //exit()
 #include <signal.h>
 
- #include <stdio.h>
- #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "DEV_Config.h"
 #include "L76X.h"
-
-void  Handler(int signo)
-{
-    //System Exit
-    printf("\r\nHandler:Program stop\r\n");
-
-    DEV_ModuleExit();
-
-    exit(0);
-}
 
 int main(int argc, char **argv)
 {
     GNRMC GPS;
     Coordinates Baidu;
-    time_t time_now;
-
-    int h,m,s;
-    int h1,m1,s1;
-    h = m = s = 0;
-
-
-    if (DEV_ModuleInit()==1)return 1;
-    // Exception handling:ctrl + c
-
-    signal(SIGINT, Handler);
 
     DEV_Delay_ms(100);
     DEV_Set_Baudrate(9600);
@@ -46,30 +25,17 @@ int main(int argc, char **argv)
     DEV_Set_Baudrate(115200);
     DEV_Delay_ms(100);
 
-
     printf("Set the output data interval to 400ms\r\n");
     L76X_Send_Command(SET_POS_FIX_400MS);
 
     //Set output message
     L76X_Send_Command(SET_NMEA_OUTPUT);
 
-    L76X_Exit_BackupMode();
-
-    time(&time_now);
-
-    h1 = time_now/3600%60;
-    m1 = time_now/60%60;
-    s1 = time_now%60;
-    if(m1 >= 59){
-        h1 = h1 + 1;
-        m1 = m1 + 1 -60;
-    }
     GPS = L76X_Gat_GNRMC();
     printf("\r\n");
     printf("Time: %d:%d:%d \r\n", GPS.Time_H, GPS.Time_M, GPS.Time_S);
     printf("Latitude and longitude: %lf  %c  %lf  %c\r\n", GPS.Lat, GPS.Lat_area, GPS.Lon, GPS.Lon_area);
     Baidu = L76X_Baidu_Coordinates();
     printf("Baudu Coordinates   %lf,%lf    \r\n", Baidu.Lat, Baidu.Lon);
-    DEV_ModuleExit();
     return 0;
 }
